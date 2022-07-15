ab2314f38bc34c2fa65b3ed84d6352071c1be6a1
glynk <glyn_knight@hotmail.com>
2022-07-15 15:59:56 +0100

    not sure why it breaks

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -34,6 +34,7 @@ const fetchData = async () => {
   }
 }
 
+
 useEffect(()=>{
 fetchData()
  },[] )
--- src/Components/ResultsDisplay/ResultsDisplay.tsx
+++ src/Components/ResultsDisplay/ResultsDisplay.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function ResultsDisplay({data}:any){
 console.log({data})
   return  <>
+  
   <h2>Weather for {data.name} </h2>
   <p>Temp {data.main.temp}</p>
   <p>Wind Speed {data.wind.speed}</p>
