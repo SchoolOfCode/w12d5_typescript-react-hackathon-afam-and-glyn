24ffd5ec11d162992ff44a11783578e82ad4bf06
glynk <glyn_knight@hotmail.com>
2022-07-15 12:30:50 +0100

    setting up states

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {useState} from 'react'
 import './App.css';
 import SearchBar from '../SearchBar';
 import ResultsDisplay from '../ResultsDisplay';
 
 function App() {
+
+
+const [weatherData, setWeatherData] = useState({})
+const [city, setCity] = useState("")
+const [search, setSearch] = useState("")
+
+
+function onChange(e: React.ChangeEvent<HTMLInputElement>): void{
+  setSearch(e.target.value)
+}
+
+
+
+
+
+
+
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
